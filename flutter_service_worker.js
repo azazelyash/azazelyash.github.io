c01100860750da5c0fba31905e43cd0c18819090
Yash Vishwakarma <jss.yash085@gmail.com>
2024-08-29 00:21:06 +0530

    Fix order screen widgets

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -287,14 +287,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b22b7c9b0f576b33b6671b273c741f25
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "8f992c77d43449ed8b6b99e8a4f9ca32",
+"flutter_bootstrap.js": "583aed1edc41d45365b81d5d5fd78d2f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "d3de55483a73eb2f10ece6b744b0e241",
 "/": "d3de55483a73eb2f10ece6b744b0e241",
-"main.dart.js": "f728258dce82211c9cb46b65b8cd9cdf",
+"main.dart.js": "234235f97b9f5fac2ea78c3503960b1c",
 "manifest.json": "723ba7c3b49bb858fe0ff309bc833823",
 "version.json": "41397cc8ad3ccb08ad6b487e3ca0e68b"};
 // The application shell files that are downloaded before a service worker can
