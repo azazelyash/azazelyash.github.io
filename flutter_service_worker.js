aac07ce7e1a87b96f2b6d73df3be593cd8d9ca09
Yash Vishwakarma <jss.yash085@gmail.com>
2026-02-01 19:13:09 +0530

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fb0148756cfe8dc27a914efa5a2a79b9",
+const RESOURCES = {"flutter_bootstrap.js": "494ab70c619e768595a1094a2cf61d74",
 "version.json": "41397cc8ad3ccb08ad6b487e3ca0e68b",
 "index.html": "b89d0493704c2b280e27a9fa9fe9ec86",
 "/": "b89d0493704c2b280e27a9fa9fe9ec86",
-"main.dart.js": "301770573daea8a54ea1ac3410fbf1a2",
+"main.dart.js": "64c07bd2b309916fdea729f3f4840ccb",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
